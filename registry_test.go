46b5f9e36b7f0431c2fa4b8b259bf9aa64f8191e
nuying117 <nuying117@gmail.com>
2017-03-01 15:10:58 +0800

    add some test cases

--- registry_test.go
+++ registry_test.go
@@ -1,6 +1,7 @@
 package funcgistry
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -14,6 +15,10 @@ func testTeardown() {
 
 func testFunc1() {
 
+}
+
+func testFunc2(a, b int) int {
+	return a + b
 }
 
 func TestAddFunction(t *testing.T) {
@@ -67,5 +72,57 @@ func TestDelFunction(t *testing.T) {
 	result = ElementExists(funcName)
 	if result != expectedExistsResultForDel {
 		t.Errorf("ElementExists(%q) == %q, expected: %q", funcName, result, expectedExistsResultForDel)
+	}
+}
+func TestCallInvalidFunction(t *testing.T) {
+	testSetup()
+	defer testTeardown()
+
+	var result bool = false
+	const funcName = "func1"
+	const expectedResult = false
+
+	_, result = Call(funcName)
+	if result != expectedResult {
+		t.Errorf("Call(%q) == %q, expected: %q", funcName, result, expectedResult)
+	}
+}
+func TestCallValidFunction(t *testing.T) {
+	testSetup()
+	defer testTeardown()
+
+	var result bool = false
+	const expectedResult = true
+	const func1Name = "func1"
+	const func2Name = "func2"
+	testFuncsData := []struct {
+		funcName          string
+		funcImpl          interface{}
+		expectedAddResult bool
+	}{
+		{"func1", testFunc1, true},
+		{"func2", testFunc2, true},
+	}
+
+	for _, testData := range testFuncsData {
+		result = AddElement(testData.funcName, testData.funcImpl)
+		if result != testData.expectedAddResult {
+			t.Errorf("AddElement(%q, %q) == %q, expected: %q", testData.funcName, testData.funcImpl, result, testData.expectedAddResult)
+		}
+	}
+
+	_, result = Call("func1")
+	if result != expectedResult {
+		t.Errorf("Call(%q) == %q, expected: %q", func1Name, result, expectedResult)
+	}
+
+	var rets []reflect.Value
+	rets, result = Call("func2", 2, 3)
+	if result != expectedResult {
+		t.Errorf("Call(%q) == %q, expected: %q", func2Name, result, expectedResult)
+	}
+
+	if rets[0].Int() != 5 {
+		t.Errorf("return value of func(%q)  == %q, expected: %q", func2Name, rets[0], 5)
 	}
 }
